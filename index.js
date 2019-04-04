11e9371b505ab96d44e82f8fcd6909c7af383cc5
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-04-04 18:17:46 +0100

    google auth

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const passport = require("passport");
+const GoogleStartegy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
+const app = express();
+
+passport.use(
+  new GoogleStartegy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecrect,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken,refrechToken,profile,done) => {
+      console.log('access',accessToken);
+      console.log('refrechtoken',refrechToken)
+      console.log('profile',profile)
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
