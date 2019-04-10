55d80caea68966e88bc2c3177934cafdf931d106
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-04-10 18:04:17 +0100

    correct the build clients commands for the heroku deployment

--- index.js
+++ index.js
@@ -35,12 +35,18 @@ app.use("/api/cars", CarRoutes);
 require("./routes/authRoutes")(app);
 require("./routes/carsRoutes")(app);
 
-if (process.env.PORT === "production") {
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
 
-  resizeBy.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
